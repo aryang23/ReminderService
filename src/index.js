d0706323c60774f2cd15200197f45c6b6ad33ea0
Aryan Grover <aryang23@Aryans-MacBook-Pro.local>
2023-07-23 15:13:19 +0530

    Setup nodemailer

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const {PORT} = require('./config/serverConfig');
+const { sendBasicEmail } = require('./services/email-service');
 
 
 const setUpAndStartServer = () => {
@@ -12,6 +13,11 @@ const setUpAndStartServer = () => {
 
     app.listen(PORT, () => {
         console.log(`Server listening on ${PORT}`);
+
+        sendBasicEmail('support@gmail.com',
+        'aryannode@gmail.com',
+        'This is a testing email',
+        'Hey hope everythings fine');
     })
 }
 
